5dbc4da7b0b5ded2548b41a872eeb13d4f2e34d4
Fyardlest <fyardlest1@gmail.com>
2020-09-02 08:14:12 -0400

    Express REST API with MongoDB and Mongoose Part 2

--- routes/hospitalRouter.js
+++ routes/hospitalRouter.js
@@ -84,4 +84,157 @@ hospitalRouter.route("/:hospitalId")
     .catch(err => next(err));
 });
 
+// Handle user comments: 
+hospitalRouter
+  .route("/:hospitalId/comments")
+  .get((req, res, next) => {
+    Hospital.findById(req.params.hospitalId)
+      .then((hospital) => {
+        if (hospital) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(hospital.comments);
+        } else {
+          err = new Error(`hospital ${req.params.hospitalId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Hospital.findById(req.params.hospitalId)
+      .then((hospital) => {
+        if (hospital) {
+          hospital.comments.push(req.body);
+          hospital
+            .save()
+            .then((hospital) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(hospital);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`hospital ${req.params.hospitalId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `PUT operation not supported on /hospitals/${req.params.hospitalId}/comments`
+    );
+  })
+  .delete((req, res, next) => {
+    Hospital.findById(req.params.hospitalId)
+      .then((hospital) => {
+        if (hospital) {
+          for (let i = hospital.comments.length - 1; i >= 0; i--) {
+            hospital.comments.id(hospital.comments[i]._id).remove();
+          }
+          hospital
+            .save()
+            .then((hospital) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(hospital);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`hospital ${req.params.hospitalId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
+
+  // Handle user commentId
+hospitalRouter
+  .route("/:hospitalId/comments/:commentId")
+  .get((req, res, next) => {
+    Hospital.findById(req.params.hospitalId)
+      .then((hospital) => {
+        if (hospital && hospital.comments.id(req.params.commentId)) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(hospital.comments.id(req.params.commentId));
+        } else if (!hospital) {
+          err = new Error(`hospital ${req.params.hospitalId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /hospitals/${req.params.hospitalId}/comments/${req.params.commentId}`
+    );
+  })
+  .put((req, res, next) => {
+    Hospital.findById(req.params.hospitalId)
+      .then((hospital) => {
+        if (hospital && hospital.comments.id(req.params.commentId)) {
+          if (req.body.rating) {
+            hospital.comments.id(req.params.commentId).rating = req.body.rating;
+          }
+          if (req.body.text) {
+            hospital.comments.id(req.params.commentId).text = req.body.text;
+          }
+          hospital
+            .save()
+            .then((hospital) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(hospital);
+            })
+            .catch((err) => next(err));
+        } else if (!hospital) {
+          err = new Error(`hospital ${req.params.hospitalId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Hospital.findById(req.params.hospitalId)
+      .then((hospital) => {
+        if (hospital && hospital.comments.id(req.params.commentId)) {
+          hospital.comments.id(req.params.commentId).remove();
+          hospital
+            .save()
+            .then((hospital) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(hospital);
+            })
+            .catch((err) => next(err));
+        } else if (!hospital) {
+          err = new Error(`hospital ${req.params.hospitalId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
+
 module.exports = hospitalRouter;
